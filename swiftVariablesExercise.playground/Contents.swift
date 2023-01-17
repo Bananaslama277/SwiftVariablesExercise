d6d19196f93fb57472344ae9ca2679430ef2a831
Bananaslama277 <tracesmith555@gmail.com>
2023-01-17 08:25:20 -0600

    Initial Commit

---
+++ swiftVariablesExercise.playground/Contents.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+print("Trace")
+
+//Int, String, Float, Double, Bool, and Character
+//Let is used for constant values and names
+
+var person: String = "Chris"
+person = "Robert"
+
+//Bonus
+var number = 3
+number = number + 2
+number += 5
+
+
